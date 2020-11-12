afd4684f94a5ed373eabf857dd12372adf83daa5
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-11-12 10:49:20 +0100

    Add files via upload

---
+++ buricMarioPonavljanje2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricMarioPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi niz znakova");
+            string s1 = Console.ReadLine();
+            s1 = s1.Replace(" ", "_");
+            
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
